7df2437621f12fae78b2b8c1c1ff0c1aef510e18
sen <yjghdsss@163.com>
2021-07-12 16:20:37 +0800

    ch1

--- os/src/lang_items.rs
+++ os/src/lang_items.rs
@@ -1,7 +1,13 @@
-// os/src/lang_items.rs
 use core::panic::PanicInfo;
+use crate::{println, sbi};
+
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!("Panicked at {}:{} {}", location.file(), location.line(), info.message().unwrap());
+    } else {
+        println!("Panicked: {}", info.message().unwrap());
+    }
+    sbi::shutdown()
 }
--- os/src/main.rs
+++ os/src/main.rs
@@ -4,92 +4,11 @@
 #![feature(global_asm)]
 #![feature(panic_info_message)]
 
+mod console;
+mod lang_items;
+mod sbi;
 
 global_asm!(include_str!("entry.asm"));
-
-// panic handler
-mod lang_items;
-
-/***********************************************************************
-system exit
-************************************************************************/
-const SYSCALL_EXIT: usize = 93;
-
-fn syscall(id: usize, args: [usize; 3]) -> isize {
-    let mut ret: isize;
-    unsafe {
-        llvm_asm!("ecall"
-            : "={x10}" (ret)
-            : "{x10}" (args[0]), "{x11}" (args[1]), "{x12}" (args[2]), "{x17}" (id)
-            : "memory"
-            : "volatile"
-        );
-    }
-    ret
-}
-
-pub fn sys_exit(xstate: i32) -> isize {
-    syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
-}
-
-/****************************************************************************
-system write
-****************************************************************************/
-const SYSCALL_WRITE: usize = 64;
-const SBI_CONSOLE_PUTCHAR: usize = 1;
-
-pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
-    syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
-}
-
-struct Stdout;
-use core::fmt;
-use core::fmt::Write;
-
-
-impl Write for Stdout {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        //sys_write(STDOUT, s.as_bytes());
-        for c in s.chars() {
-            console_putchar(c as usize);
-        }
-        Ok(())
-    }
-}
-
-pub fn print(args: fmt::Arguments) {
-    Stdout.write_fmt(args).unwrap();
-}
-
-#[macro_export]
-macro_rules! print {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
-    }
-}
-
-#[macro_export]
-macro_rules! println {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
-    }
-}
-
-pub fn console_putchar(c: usize) {
-    syscall(SBI_CONSOLE_PUTCHAR, [c, 0, 0]);
-}
-
-/********************************************************************
-system shutdown
-**********************************************************************/
-const SBI_SHUTDOWN: usize = 8;
-mod sbi;
-use sbi::sbi_call;
-
-pub fn shutdown() -> ! {
-    sbi_call(SBI_SHUTDOWN, 0, 0, 0);
-    panic!("It should shutdown!");
-}
 
 /** .bss clear */
 fn clear_bss() {
@@ -110,9 +29,30 @@ _start program entry
 // }
 
 #[no_mangle]
-pub fn rust_main() -> !{
-    loop {
-        
+pub fn rust_main() -> ! {
+    extern "C" {
+        fn stext();
+        fn etext();
+        fn srodata();
+        fn erodata();
+        fn sdata();
+        fn edata();
+        fn sbss();
+        fn ebss();
+        fn boot_stack();
+        fn boot_stack_top();
     }
-}
+    clear_bss();
 
+    println!("\x1b[31mhello world\x1b[0m");
+    println!(".text [{:#x}, {:#x}]", stext as usize, etext as usize);
+    println!(".rodata [{:#x}, {:#x}]", srodata as usize, erodata as usize);
+    println!(".data [{:#x}, {:#x}]", sdata as usize, edata as usize);
+    println!(
+        ".boot_stack [{:#x}, {:#x}]",
+        boot_stack as usize, boot_stack_top as usize
+    );
+    println!(".bss [{:#x}, {:#x}]", sbss as usize, ebss as usize);
+
+    panic!("shutdown the machine!")
+}
